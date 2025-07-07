02744089f823b480a31a0852c0b22a6b63e12d5a
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-07-07 15:15:40 +0200

    Made command_test_node easiser to use for a single joint at a time, configured by parameters

--- pkgs_control/servo_control/servo_control/test_nodes/command_test_node.py
+++ pkgs_control/servo_control/servo_control/test_nodes/command_test_node.py
@@ -12,34 +12,29 @@ class CommandTestNode(Node):
         super().__init__("command_test_node")
 
         # Parameters
-        self.declare_parameter("angle_left", 0.0)
-        self.angle_left = (
-            self.get_parameter("angle_left").get_parameter_value().double_value
-        )
+        self.declare_parameter("angle", 0)
+        angle = self.get_parameter("angle").get_parameter_value().integer_value
 
-        self.declare_parameter("angle_right", 0.0)
-        self.angle_right = (
-            self.get_parameter("angle_right").get_parameter_value().double_value
-        )
+        self.declare_parameter("topic_name", "/joint_states")
+        topic_name = self.get_parameter("topic_name").get_parameter_value().string_value
+
+        self.declare_parameter("joint_name", "joint")
+        joint_name = self.get_parameter("joint_name").get_parameter_value().string_value
 
         # Publishers
-        joint_state_pub = self.create_publisher(JointState, "/joint_states", 1)
+        joint_state_pub = self.create_publisher(JointState, topic_name, 1)
 
         # Prepare message
         joint_state_msg = JointState()
-        joint_state_msg.name = [
-            "joint_left_shoulder_pitch",
-            "joint_right_shoulder_pitch",
-        ]
-        joint_positions = [
-            np.deg2rad(self.angle_left),
-            np.deg2rad(self.angle_right),
-        ]
+        joint_state_msg.name = [joint_name]
+        joint_positions = [np.deg2rad(angle)]
         joint_state_msg.header.stamp = self.get_clock().now().to_msg()
         joint_state_msg.position = joint_positions
 
         # Publish once
-        self.get_logger().info("Published joint state command.")
+        self.get_logger().info(
+            f"Published joint state command:\ntopic_name: {topic_name}\njoint_name: {joint_name}\nangle (degrees): {angle}"
+        )
         joint_state_pub.publish(joint_state_msg)
 
         # Sleep to ensure message is sent
