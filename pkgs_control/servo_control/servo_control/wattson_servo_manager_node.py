51dd16caf5d67b309330e71ce69b8747d628a03f
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-07-29 15:24:08 +0200

    Added parameter to enable/disable finger mapping to servo range

--- pkgs_control/servo_control/servo_control/wattson_servo_manager_node.py
+++ pkgs_control/servo_control/servo_control/wattson_servo_manager_node.py
@@ -22,6 +22,13 @@ class ServoManagerNode(Node):
             self.get_parameter("control_frequency").get_parameter_value().double_value
         )
 
+        self.declare_parameter("finger_mapping_enabled", True)
+        self.finger_mapping_enabled = (
+            self.get_parameter("finger_mapping_enabled")
+            .get_parameter_value()
+            .bool_value
+        )
+
         self.declare_parameter(
             "config_folder_path",
             "install/energirobotter_bringup/share/energirobotter_bringup/config/servos",
@@ -71,15 +78,20 @@ class ServoManagerNode(Node):
         self.servo_commands_hands = dict(zip(msg.name, np.rad2deg(msg.position)))
 
         # Map angles to servo range for fingers
-        for servo_name in self.servo_commands_hands:
-            if servo_name not in self.servo_driver.servos:
-                continue
+        if self.finger_mapping_enabled:
+            for servo_name in self.servo_commands_hands:
+                if servo_name not in self.servo_driver.servos:
+                    continue
 
-            servo = self.servo_driver.servos[servo_name]
-            command = self.servo_commands_hands[servo_name]
+                servo = self.servo_driver.servos[servo_name]
+                command = self.servo_commands_hands[servo_name]
 
-            angle_mapped = self.servo_driver.map_finger_to_servo(servo, command)
-            self.servo_commands_hands[servo_name] = angle_mapped
+                self.get_logger().info(f"tracked angle: {command}")
+
+                angle_mapped = self.servo_driver.map_finger_to_servo(servo, command)
+                self.servo_commands_hands[servo_name] = angle_mapped
+
+                self.get_logger().info(f"mapped angle: {angle_mapped}")
 
     def callback_timer(self):
         # Combine command dicts into one
