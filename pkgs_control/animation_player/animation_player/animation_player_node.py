5f443a5ade340e8ac4a1af3b07784c74ee46f6ed
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-08-11 16:49:16 +0200

    Updated to use JointState message

--- pkgs_control/animation_player/animation_player/animation_player_node.py
+++ pkgs_control/animation_player/animation_player/animation_player_node.py
@@ -1,6 +1,7 @@
 import rclpy
-from rclpy import Node
+from rclpy.node import Node
 import std_msgs.msg
+from sensor_msgs.msg import JointState
 
 from animation_player.src import csv_reader
 
@@ -22,36 +23,36 @@ class AnimationPlayerNode(Node):
         # CSV file setup
         self.csv_reader = csv_reader.CSVReader(self.csv_file_path)
 
-        self.joint_idx: int = {}
-        self.joint_publishers = {}
-        for idx, joint in enumerate(self.csv_reader.get_header()):
-            self.joint_idx[joint] = idx
-            self.joint_publishers[joint] = None
+        self.joints_names = []
+        for joint in self.csv_reader.get_header():
+            self.joints_names.append(joint)
+
+        self.joints_names = self.joints_names[1:]  # Skip frame
 
         # Timers
         self.timer = self.create_timer(1.0 / self.fps, self.callback_timer)
 
         # Publishers
-        for joint in self.joint_publishers.keys():
-            self.joint_publishers[joint] = self.create_publisher(
-                std_msgs.msg.Float64, "/" + joint + "/set_error", 1
-            )
+        self.joint_state_pub = self.create_publisher(JointState, "/joint_states", 10)
 
         # Node variables
+        self.joint_state_msg = JointState()
+        self.joint_state_msg.name = self.joints_names
 
     def __del__(self):
         self.csv_reader.close()
 
     def callback_timer(self):
 
-        if None in self.joint_publishers.values():
-            return
+        row_data = self.csv_reader.get_next_row()
+        joint_data = [float(x) for x in row_data[1:]]
 
-        row = self.csv_reader.get_next_row()
+        self.joint_state_msg.header.stamp = self.get_clock().now().to_msg()
+        self.joint_state_msg.header.frame_id = row_data[0]
+        self.joint_state_msg.position = joint_data
 
-        for joint in self.joint_publishers.keys():
-            data = row[self.joint_idx[joint]]
-            self.joint_publishers[joint].publish(std_msgs.msg.Float64(data=float(data)))
+        # Publish the joint state message
+        self.joint_state_pub.publish(self.joint_state_msg)
 
 
 def main(args=None):
