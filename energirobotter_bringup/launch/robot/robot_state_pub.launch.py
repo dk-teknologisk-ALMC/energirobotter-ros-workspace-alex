c5f0a6ce631d208263e1398ba09b4b8d8ab545d7
Nicoline L. Thomsen <xnlth@energinet.dk>
2025-09-09 13:53:33 +0200

    Launch file for robot description publisher and rviz

---
+++ energirobotter_bringup/launch/robot/robot_state_pub.launch.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+package_name = "energirobotter_bringup"
+
+
+def launch_setup(context, *args, **kwargs):
+    rviz = LaunchConfiguration("rviz")
+    description_package = LaunchConfiguration("description_package")
+
+    urdf_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "urdf", "phobos_generated.urdf"]
+    ).perform(context)
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare(package_name),
+            "config",
+            "rviz",
+            "teleoperation.rviz",
+        ]
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        arguments=[urdf_file],
+        output="screen",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(rviz),
+    )
+
+    return [
+        robot_state_pub_node,
+        rviz_node,
+    ]
+
+
+def generate_launch_description():
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz",
+                default_value="false",
+                description="Start RViz2 automatically with this launch file.",
+                choices=["true", "false"],
+            ),
+            DeclareLaunchArgument(
+                "description_package",
+                default_value="wattson_description",
+                description="Package in workspace that contains robot URDF description.",
+                choices=["elrik_description", "wattson_description"],
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
